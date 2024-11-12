5c452d9ccb4e30d975c4adbaea41b9826007ff7c
Squirrel <rylancarey2011@gmail.com>
2024-11-12 12:17:03 -0800

    Update spoon.cpp

--- spoon.cpp
+++ spoon.cpp
@@ -26,12 +26,12 @@ void adoption() {
     std::cout << "You have $800, y/n\n";
     std::cin >> buy;
 
-    if (buy == "y") {
+    if (buy == "y" || buy == "yes" || buy == "Yes") {
         std::cout << "You have bought your very first pet spoon! Now that you have bought a pet spoon, it's gonna need a name. Put the name you want your spoon to have!\n\n";
         std::cin >> spoonname;
         std::cout << "You have named your spoon " << spoonname << "! Let's get some supplies so that you can take care of your spoon!";
     }
-    if (buy == "n") {
+    if (buy == "n" || buy == "no" || buy == "No") {
         std::cout << "So the thing about that is that you can't say no. You have 2 options. Buy the spoon or quit the game. (Buy or quit)\n\n";
         std::cin >> useroption;
         if (useroption == "Buy" || useroption == "buy") {
@@ -39,27 +39,48 @@ void adoption() {
     std::cout << "You have $800, y/n\n";
     std::cin >> buy2;
     
-    if (buy2 == "y") {
+    if (buy2 == "y" || buy2 == "yes" || buy2 == "Yes") {
         std::cout << "You have bought your very first pet spoon! Now that you have bought a pet spoon, it's gonna need a name. Put the name you want your spoon to have!\n\n";
         std::cin >> spoonname;
         std::cout << "You have named your spoon " << spoonname << "! Let's get some supplies so that you can take care of your spoon!";
     }
-    if (buy2 == "n") {
+    if (buy2 == "n" || buy2 == "no" || buy2 == "No") {
         std::cout << "Nope, try again.\n\n";
         std::cout << "Do you want to buy her? She's only $3\n\n";
     std::cout << "You have $800, (y)\n";
     std::cin >> buy3;
     }
     
-    if (buy3 == "y") {
+    if (buy3 == "y" || buy3 == "yes" || buy3 == "Yes") {
          std::cout << "You have bought your very first pet spoon! Now that you have bought a pet spoon, it's gonna need a name. Put the name you want your spoon to have!\n\n";
         std::cin >> spoonname;
         std::cout << "You have named your spoon " << spoonname << "! Let's get some supplies so that you can take care of your spoon!";
     }
     }
-    
     }
+    }
+    void store_at_start() {
+        double money;
+        double moneybefore;
+        moneybefore = 797;
+        std::string foodbuy;
+        std::string anything_else_option;
+        std::cout << "\n\nWelcome to the Spoon pet shop! This place will have everything you'll ever need to take care of your spoon! We also have a deal on so it's your lucky day!\n\n";
+        std::cout << "We'll start off by buying some food for your pet spoon.\n\n";
+        std::cout << "School pizza(1)      Bread(2)       Watermelon(3)\n";
+        std::cout << "     $3            $1             $3            \n";
+        std::cin >> foodbuy;
+        if (foodbuy == "1") {
+            std::cout  << "You have bought a slice of school pizza!\n";
+            money = moneybefore - 3;
+            std::cout << "You now have $" << money << " left!";
+            
         }
+        std::cout << "\nanything else?";
+        std::cin >> anything_else_option;
+        
+        
+    }
     
          
      
@@ -86,5 +107,6 @@ std::string ok;
        adoption();
    }
     viewingarea();
+    store_at_start();
     return 0;
 }
