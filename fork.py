541b5d3bfe83b3b6becb1370334c0f995eae9158
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-12 15:27:29 -0600

    Create fork.py

---
+++ fork.py
@@ -0,0 +1,6 @@
+print("Welcome to FORK")
+print("SPOON EXPANSION PACK")
+print("RUN 'fork' IN SPOON TO MERGE FOR A SPECIAL PET")
+with open('fork.txt', 'w') as f:
+    f.write('This user has used the FORK expansion.')
+    f.close()
