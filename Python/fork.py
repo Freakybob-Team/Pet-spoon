19d2eca530299f4810eea7788f5a7114c25af96e
Wish <100239673+wish13yt@users.noreply.github.com>
2024-11-13 17:22:08 -0600

    modified:   Python/fork.py

--- Python/fork.py
+++ Python/fork.py
@@ -5,9 +5,14 @@ def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 def eat():
     print("Oh no! " + name + " is hungry!")
-    food = input("You currently have 1 'PH'! Would you like to feed them this?")
+    food = input("You currently have 1 'PH'! Would you like to feed them this? (Yes/No) ")
     if ("Yes" in food):
         print(name + " is happy!")
+    if ("No" in food):
+        print(name + " is not satisfied with your care! They have returned to the wild and deleted your FORK file.")
+        os.remove("fork.txt")
+        print("You know what? I'm pissed too.")
+        os.remove("Python/fork.py")
 print("Welcome to FORK")
 print("SPOON EXPANSION PACK")
 print("RUN 'fork' IN SPOON TO MERGE FOR A SPECIAL PET")
@@ -27,3 +32,4 @@ if (doEat == 2):
 if (doEat == 1):
     print("Your fork is satisfied.")
     time.sleep(2)
+    eat()
