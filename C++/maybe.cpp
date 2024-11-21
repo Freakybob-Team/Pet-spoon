41cf6dd4d69b4448cee69af1064bcd296159de26
Squirrel <rylancarey2011@gmail.com>
2024-11-21 14:13:23 -0800

    Create maybe.cpp

---
+++ C++/maybe.cpp
@@ -0,0 +1,40 @@
+/*if (options == "shop" || options == "Shop") {
+        std::cout << "You're now in the shop.What would you like to buy?\n";
+        std::cout << "1.Squishmallow    2.Freakybob toy\n";
+        std::cout << "10$                5$\n";
+        std::cin >> shop_options;
+        if (shop_options == "1") {
+            if (money < 10) {
+                std::cout << "not enough money!\n";
+                game();
+            }
+            if (money >= 10) {
+                money = money - 10;
+                std::cout << "you bought a squishmallow.\n";
+                std::cout << "you now have " << money << "$\n";
+                game();
+            }
+        }
+        if (shop_options == "2") {
+            if (money < 5) {
+                std::cout << "not enough money!\n";
+                game();
+            }
+            if (money >= 5) {
+                std::cout << "You bought a freakybob toy\n";
+                money = money - 5;
+                std::cout << "you now have " << money << "$\n";
+                game();
+            }
+        }
+    }
+        else {
+            game();
+        }
+    }
+                else {
+                    game();
+                }
+
+                }
+*/
