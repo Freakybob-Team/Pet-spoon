05234783438cb4871ece2b86a0e743d5996a8c69
Squirrel <rylancarey2011@gmail.com>
2024-11-21 20:14:00 -0800

    Create Spoon.py

---
+++ Python/Spoon.py
@@ -0,0 +1,6 @@
+# spoonname = ""
+# xp = 0
+# username = ""
+# money = 777
+# **************************************************************
+# Wish, we can both try to cook on this, it would be so cool plz (I'll try to help at least :sob:)
