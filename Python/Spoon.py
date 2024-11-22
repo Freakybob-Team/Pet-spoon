f503b202ee6d70a40598f32240aa3853588cf3e0
Squirrel <rylancarey2011@gmail.com>
2024-11-22 08:45:28 -0800

    Update Spoon.py

--- Python/Spoon.py
+++ Python/Spoon.py
@@ -8,13 +8,22 @@ def adoption():
     
 
 def main():
-        print(" SSSSS    PPPPP   OOO   OOO   N   N ")
-        print("S         P   P  O   O O   O  NN  N ")
-        print(" SSSSS    PPPPP  O   O O   O  N N N ")
-        print("     S    P      O   O O   O  N  NN ")
-        print(" SSSSS    P       OOO   OOO   N   N ")
-        print("Made by 5quirre1")
+	
+    print(" SSSSS    PPPPP   OOO   OOO   N   N ")
+    print("S         P   P  O   O O   O  NN  N ")
+    print(" SSSSS    PPPPP  O   O O   O  N N N ")
+    print("     S    P      O   O O   O  N  NN ")
+    print(" SSSSS    P       OOO   OOO   N   N ")
+    print("Made by 5quirre1")
+    print("-------------------------------------------------------")
+    print("Welcome to Spoon. Say 'Info' for what Spoon is about.\nSay anything else to continue on your adventure of taking care of a Spoon greg\n")
+    what = input()
+    if (what == 'info' or what == 'Info' or what == 'INFO'):
+        print("\nThis game is about taking care of a pet spoon. Don't ask why cause like, it's very useful lmao\nSome stuff that is going to be updated in later updates:\n\nA saving system\nRandom events when you like use the command 'Check' or something\nRandom cool stuff\nDo You want to know some bugs that have been found? (Say 'Bugs')\n")
+    ok = input()
+    if (ok == 'bugs' or ok == 'Bugs'):
         print("-------------------------------------------------------")
+        print("If You say anything else to the Buying of the Spoon part, it will completly skip that :sob:\nThat's all for now...\n")
 
 if __name__=="__main__":
     main()
