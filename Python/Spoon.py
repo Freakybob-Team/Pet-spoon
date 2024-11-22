6c3763a4a2e7ead82cd5b674a2aaa9dce8555b10
Squirrel <rylancarey2011@gmail.com>
2024-11-22 08:22:45 -0800

    Update and rename REALSpoon.py to Spoon.py

---
+++ Python/Spoon.py
@@ -0,0 +1,20 @@
+spoonname = ""
+xp = 0
+username = ""
+money = 777
+ 
+def adoption():
+    print("greg")
+    
+
+def main():
+        print(" SSSSS    PPPPP   OOO   OOO   N   N ")
+        print("S         P   P  O   O O   O  NN  N ")
+        print(" SSSSS    PPPPP  O   O O   O  N N N ")
+        print("     S    P      O   O O   O  N  NN ")
+        print(" SSSSS    P       OOO   OOO   N   N ")
+        print("Made by 5quirre1")
+        print("-------------------------------------------------------")
+
+if __name__=="__main__":
+    main()
